ed7de2059b949283f4b684b4e7994212ba38c1fd
ojaswa1942 <ojaswa1942@gmail.com>
2018-05-29 03:04:27 +0530

    Began2

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card =() => {
+const Card =(props) => {
 	return (
 		<div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img src ='https://robohash.org/pixr?200x200' alt='Pixr' />
+			<img  alt='Pixr' src ={`https://robohash.org/${props.id}?200x200`} />
 			<div>
-			<h2>Pixr</h2>
-			<p>pixr@gmail.com</p>
+			<h2>{props.name}</h2>
+			<p>{props.email}</p>
 			</div>
 		</div>
 		);
--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import Card from './Card';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
+import {robots} from './robots'; //{} because default isn't used while exporting, destructure for multiple
 
 ReactDOM.render(
 	<div>
-		<Card />
-		<Card />
-		<Card />
+		<Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+		<Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+		<Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
 	</div>,
 	document.getElementById('root'));
 registerServiceWorker();
