7d91bca220f5e20b570c87c9b1b31be33f930673
ojaswa1942 <ojaswa1942@gmail.com>
2018-05-29 03:10:05 +0530

    destruct

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const Card =(props) => {
+const Card =({name, email, id}) => {
+	//const {name,email,id} = props; //for removing props.
 	return (
 		<div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img  alt='Pixr' src ={`https://robohash.org/${props.id}?200x200`} />
+			<img  alt='Pixr' src ={`https://robohash.org/${id+23}?200x200`} />
 			<div>
-			<h2>{props.name}</h2>
-			<p>{props.email}</p>
+			<h2>{name}</h2>
+			<p>{email}</p>
 			</div>
 		</div>
 		);
