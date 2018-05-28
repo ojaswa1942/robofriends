cbce4c9a03eeb0fb91f87f4543fe415360914dd4
ojaswa1942 <ojaswa1942@gmail.com>
2018-05-29 02:46:09 +0530

    Began1

--- src/Card.js
+++ src/Card.js
@@ -1,2 +1,15 @@
 import React from 'react';
- {components}
+
+const Card =() => {
+	return (
+		<div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+			<img src ='https://robohash.org/pixr?200x200' alt='Pixr' />
+			<div>
+			<h2>Pixr</h2>
+			<p>pixr@gmail.com</p>
+			</div>
+		</div>
+		);
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -5,5 +5,11 @@ import Card from './Card';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 
-ReactDOM.render(<Card />, document.getElementById('root'));
+ReactDOM.render(
+	<div>
+		<Card />
+		<Card />
+		<Card />
+	</div>,
+	document.getElementById('root'));
 registerServiceWorker();
