63de9fb241979618dfa1e68bbee88d147f398469
ojaswa1942 <ojaswa1942@gmail.com>
2018-05-29 21:31:46 +0530

    Searchbox Working

--- src/App.js
+++ src/App.js
@@ -4,14 +4,29 @@ import {robots} from './robots'; //{} because default isn't used while exporting
 import SearchBox from './SearchBox';
 
 class App extends Component {
+	constructor(){
+		super();
+		this.state = {
+			robots: robots,
+			searchfield: ''
+		}
+	}
 
-	
+	onSearchChange = (event) => {
+		this.setState({searchfield:event.target.value})
+		// console.log(searchedRobots);
+	}
+
+
 	render() {
+		const searchedRobots = this.state.robots.filter(robot =>{
+			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		})
 		return(
 			<div className='tc'>
 				<h1>RoboFriends</h1>
-				<SearchBox />
-				<CardList robots={robots} />
+				<SearchBox searchChange={this.onSearchChange}/>
+				<CardList robots={searchedRobots} />
 			</div>
 			);
 	}
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = ({searchfield, searchChange}) => {
 	return(
 	<div className='pa2'>
-		<input className='pa3 ba b--green bg-lightest-blue' type='search' placeholder='Search friends' />
-	
+		<input 
+			className='pa3 ba b--green bg-lightest-blue' 
+			type='search' 
+			placeholder='Search friends' 
+			onChange={searchChange}
+			/>	
 	</div>
 	);
 }
