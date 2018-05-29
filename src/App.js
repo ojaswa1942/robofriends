7809e43f7a67e5ee58c20e0d8874ef6ef6bf5968
ojaswa1942 <ojaswa1942@gmail.com>
2018-05-30 00:50:47 +0530

    API for robots

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
-import {robots} from './robots'; //{} because default isn't used while exporting, destructure for multiple
+//Now use API
+// import {robots} from './robots'; //{} because default isn't used while exporting, destructure for multiple
 import SearchBox from './SearchBox';
 import './App.css';
 
@@ -8,10 +9,17 @@ class App extends Component {
 	constructor(){
 		super();
 		this.state = {
-			robots: robots,
+			robots: [],
 			searchfield: ''
 		}
 	}
+
+	componentDidMount(){
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(users => this.setState({robots:users}))
+	}
+
 
 	onSearchChange = (event) => {
 		this.setState({searchfield:event.target.value})
@@ -23,13 +31,16 @@ class App extends Component {
 		const searchedRobots = this.state.robots.filter(robot =>{
 			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
 		})
-		return(
-			<div className='tc'>
-				<h1 className='f1'>RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange}/>
-				<CardList robots={searchedRobots} />
-			</div>
-			);
+		if(this.state.robots.length===0)
+			return (<h1 className="tc f2">Loading</h1>);
+		else{
+			return(
+				<div className='tc'>
+					<h1 className='f1'>RoboFriends</h1>
+					<SearchBox searchChange={this.onSearchChange}/>
+					<CardList robots={searchedRobots} />
+				</div>
+				);}
 	}
 }
 
